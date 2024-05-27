b11d76266ff47c1076828ec16e3d86d6a8d39322
WaveGuide1 <barthalaekwe@gmail.com>
2024-05-27 19:30:22 +0100

    initial commit

---
+++ src/test/java/io/waveguide/movies/MoviesApplicationTests.java
@@ -0,0 +1,13 @@
+package io.waveguide.movies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
