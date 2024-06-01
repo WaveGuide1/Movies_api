7d14b8c78c0e65f1ffa7114e3d7df8f8e7b00332
WaveGuide1 <barthalaekwe@gmail.com>
2024-06-01 18:20:46 +0100

    Movie service

---
+++ src/main/java/io/waveguide/movies/movie/MovieService.java
@@ -0,0 +1,15 @@
+package io.waveguide.movies.movie;
+
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.List;
+
+public interface MovieService {
+
+    MovieRequest addMovie(MovieRequest request, MultipartFile file) throws IOException;
+
+    MovieRequest getMovie(Long movieId);
+
+    List<MovieRequest> getAllMovies();
+}
---
+++ src/main/java/io/waveguide/movies/movie/MovieServiceImp.java
@@ -0,0 +1,64 @@
+package io.waveguide.movies.movie;
+
+import io.waveguide.movies.file.FileService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class MovieServiceImp implements MovieService{
+
+    private final MovieRepository movieRepository;
+
+    private final FileService fileService;
+
+    @Value("${base.url}")
+    private String baseUrl;
+
+    @Value("${project.poster}")
+    private String path;
+
+    @Override
+    public MovieRequest addMovie(MovieRequest request, MultipartFile file) throws IOException {
+
+        // Upload the file
+        String uploadFileName = fileService.uploadFile(path, file);
+
+        request.setPoster(uploadFileName);
+
+        var movie = Movie.builder()
+                .title(request.getTitle())
+                .director(request.getDirector())
+                .studio(request.getStudio())
+                .movieCast(request.getMovieCast())
+                .poster(request.getPoster())
+                .releaseYear(request.getReleaseYear())
+                .build();
+        Movie savedMovie = movieRepository.save(movie);
+
+        // Generating posterUrl
+        String posterUrl = "/files/" + uploadFileName;
+
+        MovieRequest movieRequest = new MovieRequest();
+
+        BeanUtils.copyProperties(savedMovie, movieRequest);
+        movieRequest.setPosterUrl(posterUrl);
+        return movieRequest;
+    }
+
+    @Override
+    public MovieRequest getMovie(Long movieId) {
+        return null;
+    }
+
+    @Override
+    public List<MovieRequest> getAllMovies() {
+        return List.of();
+    }
+}
