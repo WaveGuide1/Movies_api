404cdaeaf3497240df50c0c2c24266344201c02e
WaveGuide1 <barthalaekwe@gmail.com>
2024-06-02 21:30:32 +0100

    Fixed movie controller error

--- src/main/java/io/waveguide/movies/movie/MovieController.java
+++ src/main/java/io/waveguide/movies/movie/MovieController.java
@@ -6,17 +6,14 @@ import io.waveguide.movies.utils.GeneralResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestPart;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("api/v1/movies")
+@RequestMapping("/api/v1/movies")
 public class MovieController {
 
     private final MovieServiceImp movieServiceImp;
