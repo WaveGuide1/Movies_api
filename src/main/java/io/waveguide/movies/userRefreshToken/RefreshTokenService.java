75e0158164ec126839c5507842a4c3052b150c40
WaveGuide1 <barthalaekwe@gmail.com>
2024-06-24 04:04:38 +0100

    Jwt filter

---
+++ src/main/java/io/waveguide/movies/userRefreshToken/RefreshTokenService.java
@@ -0,0 +1,49 @@
+package io.waveguide.movies.userRefreshToken;
+
+import io.waveguide.movies.user.User;
+import io.waveguide.movies.user.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.time.Instant;
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
+public class RefreshTokenService {
+
+    private final UserRepository userRepository;
+    private final RefreshTokenRepository refreshTokenRepository;
+
+    public RefreshToken createRefreshToken(String username){
+        User user = userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+
+        RefreshToken refreshToken = user.getRefreshToken();
+
+        if(refreshToken == null){
+            long validTime = 6*60*60*10000;
+            refreshToken = RefreshToken.builder()
+                    .refreshToken(UUID.randomUUID().toString())
+                    .expirationTime(Instant.now().plusMillis(validTime))
+                    .user(user)
+                    .build();
+
+            refreshTokenRepository.save(refreshToken);
+        }
+        return refreshToken;
+    }
+
+    public RefreshToken verifyRefreshToken(String token){
+        RefreshToken refreshToken = refreshTokenRepository.findByRefreshToken(token)
+                .orElseThrow(() -> new RuntimeException("Refresh token not found"));
+
+        if(refreshToken.getExpirationTime().compareTo(Instant.now()) < 0){
+            refreshTokenRepository.delete(refreshToken);
+            throw new RuntimeException("Refresh token expired");
+        }
+
+        return refreshToken;
+    }
+}
